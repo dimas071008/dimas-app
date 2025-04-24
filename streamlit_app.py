882ab0ae75b1d4e12d7cf9d4f2a80573c6677500
dimas071008 <dimassatriabayu@gmail.com>
2025-04-24 11:01:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Latihan membuat Web Maniezzzkuu")
+st.title("Latihan membuat Web Maniezzzkuu💋")
 st.write(
     "Latihan membuat website melalui Github dan Streamlit dengan menggunakan bahasa Python"
 )
