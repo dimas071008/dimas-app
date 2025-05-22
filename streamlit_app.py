b52340821ae7de3ea8d609aa7671734a621aafd0
dimas071008 <dimassatriabayu@gmail.com>
2025-05-22 09:58:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("Latihan membuat Web Dimz")
 st.write(
     "Latihan membuat website melalui Github dan Streamlit dengan menggunakan bahasa Python"
 )
-st.import("ripe-apple-royalty-free-image-1659454396.jpg")
+st.import("ripe-apple-royalty-free-image-1659454396.jpg" , width=200)
